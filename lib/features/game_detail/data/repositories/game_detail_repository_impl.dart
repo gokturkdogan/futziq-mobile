d5ec084b20f2bd02ea3a1b8c4f7efea3ec4f11ef
TayfurSafakGencay <safak.gencay@gmail.com>
2026-07-26 22:27:46 +0300

    game detail repository impl

---
+++ lib/features/game_detail/data/repositories/game_detail_repository_impl.dart
@@ -0,0 +1,14 @@
+﻿import '../../domain/entities/game_detail.dart';
+import '../../domain/repositories/game_detail_repository.dart';
+import '../datasources/game_detail_remote_data_source.dart';
+
+class GameDetailRepositoryImpl implements GameDetailRepository {
+  final GameDetailRemoteDataSource remoteDataSource;
+
+  GameDetailRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<GameDetail> getGameDetail(String familyCode) async {
+    return await remoteDataSource.getGameDetail(familyCode);
+  }
+}
